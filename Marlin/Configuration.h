945ff925167d46b64a026fa19ead57b2b1c521f2
Melangris <113649298+Melangris@users.noreply.github.com>
2023-02-27 19:44:08 +0100

    Printer customizations update
    Actual e-steps and StallGuard values coded in config files.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1226,7 +1226,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 691.5}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 684.65}
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
